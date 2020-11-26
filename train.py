1f728e70224853d8e09c16b79d33bd95f7e18f74
yeyupiaoling <yeyupiaoling@foxmail.com>
2020-11-26 18:52:49 +0800

    添加注释

--- train.py
+++ train.py
@@ -37,21 +37,21 @@ def get_args():
     return args
 
 
-def train(opt):
+def train(args):
     # 固定初始化状态
     if torch.cuda.is_available():
         torch.cuda.manual_seed(123)
     else:
         torch.manual_seed(123)
     # 创建保存日志的文件夹
-    if os.path.isdir(opt.log_path):
-        shutil.rmtree(opt.log_path)
-    os.makedirs(opt.log_path)
+    if os.path.isdir(args.log_path):
+        shutil.rmtree(args.log_path)
+    os.makedirs(args.log_path)
     # 创建保存模型的文件夹
-    if not os.path.isdir(opt.saved_path):
-        os.makedirs(opt.saved_path)
+    if not os.path.isdir(args.saved_path):
+        os.makedirs(args.saved_path)
     # 创建多进程的游戏环境
-    envs = MultipleEnvironments(opt.world, opt.stage, opt.action_type, opt.num_processes)
+    envs = MultipleEnvironments(args.world, args.stage, args.action_type, args.num_processes)
     # 创建模型
     model = PPO(envs.num_states, envs.num_actions)
     # 使用 GPU训练
@@ -60,13 +60,13 @@ def train(opt):
     model.share_memory()
     # 为游戏评估单独开一个进程
     mp = _mp.get_context("spawn")
-    process = mp.Process(target=eval, args=(opt, model, envs.num_states, envs.num_actions))
+    process = mp.Process(target=eval, args=(args, model, envs.num_states, envs.num_actions))
     process.start()
     # 创建优化方法
-    optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
     # 刚开始给每个进程的游戏执行初始化
     [agent_conn.send(("reset", None)) for agent_conn in envs.agent_conns]
-    #
+    # 获取游戏初始的界面
     curr_states = [agent_conn.recv() for agent_conn in envs.agent_conns]
     curr_states = torch.from_numpy(np.concatenate(curr_states, 0))
     if torch.cuda.is_available():
@@ -80,23 +80,31 @@ def train(opt):
         states = []
         rewards = []
         dones = []
-        for _ in range(opt.num_local_steps):
+        for _ in range(args.num_local_steps):
             states.append(curr_states)
+            # 执行预测
             logits, value = model(curr_states)
+            # 计算每个动作的概率值
             policy = F.softmax(logits, dim=1)
-            values.append(value.squeeze())
+            # 根据每个标签的概率随机生成符合概率的标签
             old_m = Categorical(policy)
             action = old_m.sample()
+            # 记录预测数据
             actions.append(action)
+            values.append(value.squeeze())
+            #
             old_log_policy = old_m.log_prob(action)
             old_log_policies.append(old_log_policy)
+            # 向各个进程游戏发送动作
             if torch.cuda.is_available():
                 [agent_conn.send(("step", act)) for agent_conn, act in zip(envs.agent_conns, action.cpu())]
             else:
                 [agent_conn.send(("step", act)) for agent_conn, act in zip(envs.agent_conns, action)]
-
+            # 将多进程的游戏数据打包
             state, reward, done, info = zip(*[agent_conn.recv() for agent_conn in envs.agent_conns])
+            # 进行数据转换
             state = torch.from_numpy(np.concatenate(state, 0))
+            # 转换为pytorch数据
             if torch.cuda.is_available():
                 state = state.cuda()
                 reward = torch.cuda.FloatTensor(reward)
@@ -104,10 +112,11 @@ def train(opt):
             else:
                 reward = torch.FloatTensor(reward)
                 done = torch.FloatTensor(done)
+            # 记录预测数据
             rewards.append(reward)
             dones.append(done)
             curr_states = state
-
+        # 根据上面最后的图像预测
         _, next_value, = model(curr_states)
         next_value = next_value.squeeze()
         old_log_policies = torch.cat(old_log_policies).detach()
@@ -117,31 +126,34 @@ def train(opt):
         gae = 0
         R = []
         for value, reward, done in list(zip(values, rewards, dones))[::-1]:
-            gae = gae * opt.gamma * opt.tau
-            gae = gae + reward + opt.gamma * next_value.detach() * (1 - done) - value.detach()
+            gae = gae * args.gamma * args.tau
+            gae = gae + reward + args.gamma * next_value.detach() * (1 - done) - value.detach()
             next_value = value
             R.append(gae + value)
         R = R[::-1]
         R = torch.cat(R).detach()
         advantages = R - values
-        for i in range(opt.num_epochs):
-            indice = torch.randperm(opt.num_local_steps * opt.num_processes)
-            for j in range(opt.batch_size):
+        for i in range(args.num_epochs):
+            indice = torch.randperm(args.num_local_steps * args.num_processes)
+            for j in range(args.batch_size):
                 batch_indices = indice[
-                                int(j * (opt.num_local_steps * opt.num_processes / opt.batch_size)): int((j + 1) * (
-                                        opt.num_local_steps * opt.num_processes / opt.batch_size))]
+                                int(j * (args.num_local_steps * args.num_processes / args.batch_size)): int((j + 1) * (
+                                        args.num_local_steps * args.num_processes / args.batch_size))]
+                # 根据拿到的图像执行预测
                 logits, value = model(states[batch_indices])
+                # 计算每个动作的概率值
                 new_policy = F.softmax(logits, dim=1)
                 new_m = Categorical(new_policy)
+                # 计算损失
                 new_log_policy = new_m.log_prob(actions[batch_indices])
                 ratio = torch.exp(new_log_policy - old_log_policies[batch_indices])
                 actor_loss = -torch.mean(torch.min(ratio * advantages[batch_indices],
-                                                   torch.clamp(ratio, 1.0 - opt.epsilon, 1.0 + opt.epsilon) *
-                                                   advantages[
-                                                       batch_indices]))
+                                                   torch.clamp(ratio, 1.0 - args.epsilon, 1.0 + args.epsilon) *
+                                                   advantages[batch_indices]))
                 critic_loss = F.smooth_l1_loss(R[batch_indices], value.squeeze())
                 entropy_loss = torch.mean(new_m.entropy())
-                total_loss = actor_loss + critic_loss - opt.beta * entropy_loss
+                total_loss = actor_loss + critic_loss - args.beta * entropy_loss
+                # 计算梯度
                 optimizer.zero_grad()
                 total_loss.backward()
                 torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
@@ -150,5 +162,5 @@ def train(opt):
 
 
 if __name__ == "__main__":
-    opt = get_args()
-    train(opt)
+    args = get_args()
+    train(args)
